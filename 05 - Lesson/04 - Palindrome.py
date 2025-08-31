57e47b4319878bed79d559e207791fc2c3c40700
COMM_x_GITA <BigGeorgian@gmail.com>
2025-08-31 18:19:22 +0400

    შემდეგი სამი დავალება

--- 05 - Lesson/04 - Palindrome.py
+++ 05 - Lesson/04 - Palindrome.py
@@ -8,48 +8,49 @@ def palindrome(user_input):
     inverted_word = user_input[::-1]
     return inverted_word
 
-def suggestion(user_input, inverted_word):
+def suggestion(user_input):
     list1 = list(user_input)
-    list2 = list(inverted_word)
-
+    word_length = len(list1)
     mismatches_list = []
     mismatch_count = 0
-    word_length = len(list1)
+
+    is_even = (word_length % 2 == 0)
+
+    if is_even:
+        user_word_half = word_length // 2
+        temp_list = list1[:user_word_half]
+        reversed_list = temp_list[::-1]
+        list2 = temp_list + reversed_list
+    else:
+        user_word_half = word_length % 2 + word_length // 2
+        temp_list = list1[:user_word_half]
+        reversed_list = temp_list[::-1]
+        list2 = temp_list[:-1] + reversed_list
 
     for i in range(word_length):
         if list1[i] != list2[i]:
             mismatches_list.append(i)
             mismatch_count += 1
 
-    fixable = False
-    is_even = (word_length % 2 == 0)
-
-    if is_even and mismatch_count <= 3:
-        fixable = True
-    elif not is_even and mismatch_count <= 2:
-        fixable = True
-
-    if fixable:
+    if mismatch_count == 1:
         display = []
         for i, char in enumerate(user_input):
             if i in mismatches_list:
                 display.append(f"░")
             else:
                 display.append(char)
-        print("\nპალინდრომი მიიღება გაფერადებული უჯრების ცვლილებით:")
-        print(display)
+
+        print("\nპალინდრომი მიიღება გაფერადებული უჯრის ცვლილებით:")
+        print(f" Input:  {" ".join(list1)}")
+        print(f"Change:  {" ".join(display)}")
+        print(f"Result:  {" ".join(list2)}")
     else:
-        print("\nშედეგი: ერთი ცვლილებით პალინდრომი არ მიიღება.")
-    # print(f"fixable = {fixable}")
-    # print(f"is even = {is_even}")
-    # print(f"mismatches_list = {mismatches_list}")
-    # print(f"mismatch_count  = {mismatch_count}")
-    return
+        print("სამწუხაროდ ერთი ცვლილებით პალინდრომი არ მიიღება.")
+
 
 print("ამ პროგრამაში ვეცდებით შევამოწმოთ, შეყვანილი სიტყვა პალინდრომია თუ არა")
-
 while True:
-    user_input = input("შეიყვანეთ შესამოწმებელი სიტყვა: ")
+    user_input = (input("შეიყვანეთ შესამოწმებელი სიტყვა: ").lower())
     word_count = len(user_input.split())
     if user_input.isalpha() and word_count == 1:
         if user_input == palindrome(user_input):
@@ -57,7 +58,7 @@ while True:
             break
         else:
             print("ეს სიტყვა პალინდრომი არ არის")
-            suggestion(user_input, palindrome(user_input))
+            suggestion(user_input)
             break
     else:
         print("გთხოვთ შეიყვანოთ მხოლოდ ერთი სიტყვა.\n")
